75ee7f8394e91ab3f017fd98293d524e07065b5e
Ibrahima Tounkang Balde <ibrahimatounkangbalde@gmail.com>
2022-08-04 00:50:19 +0200

    Creation du jeu de dés avec l'opérateur de supériorité

---
+++ chapitre 1/exemple7.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $roll = rand (1, 6);
+
+    echo "<p>You rolled a" . $roll . "</p>";
+    if ($roll > 4) {
+        echo "<p>You win!</p>";
+        }
+        else {
+        echo "<p>Sorry, you didn't win. Better luck next time!</p>";
+        }
+    ?>
+</body>
+</html>
